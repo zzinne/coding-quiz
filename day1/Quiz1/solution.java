2a2933fcdcedc7777ca2fd382fdaaa45edbd76ca
zzinne <102705172+zzinne@users.noreply.github.com>
2022-04-05 14:04:33 +0900

    Update solution.java

--- day1/Quiz1/solution.java
+++ day1/Quiz1/solution.java
@@ -1,36 +1,15 @@
 public class MyClass {
-            public static String tripleTrouble(String input[]){
-            String result = "";
-            int length = input[0].length();
-            String[] test = new String[length];
-            
-            for(int i = 0; i <input.length; i++ ){
-               for(int x = 0; x <length; x++  ){
-                   
-                   String str = "";
-                   str = input[i];
-                   if(i == 0){
-                       test = str.split("");
-                       break;
-                   }else{
-                       String str2 = test[x];
-                       str2 = str2+str.substring(x,x+1);
-                        test[x] = str2;
-                   }
-               }
-            }
-            result = String.join("",test);
-            //Arrays.stream(test).collect(Collectors.joining());
-            //System.out.println(result);
-            return result;
+     public static String tripleTrouble(String f,String s,String t){
+        String result = "";
+        for(int i = 0; i < f.length(); i++){
+            result = result+f.charAt(i)+s.charAt(i)+t.charAt(i);
         }
+        return result;
+        
+     }
     public static void main(String args[]) {
         
-        String ex[] = {"this","test","lock"};
-        String text = tripleTrouble(ex);
-        System.out.println(text);
-        
-        
 
+      System.out.println(tripleTrouble("aa","bb","cc"));
     }
 }
