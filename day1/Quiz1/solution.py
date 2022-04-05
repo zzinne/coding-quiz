afeb5411b011673a3a9cb5a2edb8d7eb0cc590ec
zzinne <102705172+zzinne@users.noreply.github.com>
2022-04-05 14:13:31 +0900

    Update solution.py

--- day1/Quiz1/solution.py
+++ day1/Quiz1/solution.py
@@ -1,18 +1,10 @@
-import itertools
-
-def tripleTrouble(matrix):
-    result_map = []
-    for x in matrix:
-        line = []
-        for i in x:
-            line.append(i)
-        result_map.append(line)
-    
-    list2 = list(itertools.chain(*list(map(list,zip(*result_map)))))
-    result = "".join(list2)
+def repeatStr(f,s,t):
+    result = ""
+    for i,j,x in zip(f,s,t):
+        result = result+i+j+x
+        
     print(result)
 
 if __name__ == "__main__":
-
-    m = ["aa","bb","cc"]
-    tripleTrouble(m)
+    
+    repeatStr("aa","bb","cc")
